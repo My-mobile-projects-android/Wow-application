cc7e7ea7d9e751fc22db8e861383b8b7551ddd37
unknown <loulauriatzahouly@gmail.com>
2023-11-28 18:31:25 +0000

    first commit

---
+++ app/src/main/java/wow/startup/ci/MainActivity.java
@@ -0,0 +1,14 @@
+package wow.startup.ci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_welcome);
+    }
+}
---
+++ app/src/main/java/wow/startup/ci/WelcomeFragment.java
@@ -0,0 +1,30 @@
+package wow.startup.ci;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WelcomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WelcomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+}
